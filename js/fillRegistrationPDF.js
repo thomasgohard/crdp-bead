f4650a9717c2369779ecbe29c1dbcb48140a50f9
Thomas Gohard <thomasgohard@users.noreply.github.com>
2021-02-18 11:42:25 -0500

    Testing

--- js/fillRegistrationPDF.js
+++ js/fillRegistrationPDF.js
@@ -137,6 +137,7 @@ function generateQRCode() {
 
     qrcode.makeCode(qrcodeData);
     console.log("generateQRCode end");
+    console.log(document.getElementById("qrcode").getElementsByTagName("img")[0].src);
 }
 
 function clearQRCode() {
